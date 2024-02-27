9927be3a106934a79e36536a3c6d4b3407a49104
Alkimoru <plehanovdaniil91@gmail.com>
2024-02-27 11:09:09 +0300

    Добавил задания

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+void PrintNumbersA(int a)
+{
+    if (a < b - 1)
+    {
+        a++;
+        Console.Write($"{a}, ");
+        PrintNumbersA(a);
+    }
+}
+void PrintNumbersB(int b)
+{
+    if (b < a - 1)
+    {
+        b++;
+        Console.Write($"{b}, ");
+        PrintNumbersB(b);
+    }
+}
+if (a < b)
+{
+    PrintNumbersA(a);
+}
+else
+{
+    PrintNumbersB(b);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+int index = array.Length;
+void Print(int[] array)
+{
+    if (index > 0)
+    {
+        index--;
+        Console.Write(array[index]);
+        Print(array);
+    }
+}
+Print(array);
